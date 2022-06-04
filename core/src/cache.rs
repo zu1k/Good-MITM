d9a7bede38d753d47693380d58cf88ec25587217
zu1k <i@zu1k.com>
2022-06-04 13:57:57 +0800

    feat: Cache regex
    Signed-off-by: zu1k <i@zu1k.com>

--- core/src/cache.rs
+++ core/src/cache.rs
@@ -1,5 +1,11 @@
+use cached::{proc_macro::cached, SizedCache};
 use fancy_regex::Regex;
 
+#[cached(
+    type = "SizedCache<String, Regex>",
+    create = "{ SizedCache::with_size(100) }",
+    convert = r#"{ re.to_string() }"#
+)]
 pub fn get_regex(re: &str) -> Regex {
     fancy_regex::Regex::new(re).unwrap()
 }
