83249812534235e141bd9087f3548018bc215fd0
zu1k <i@zu1k.com>
2022-06-04 20:31:35 +0800

    feat: Trust all server cert
    Signed-off-by: zu1k <i@zu1k.com>

--- core/src/http_client.rs
+++ core/src/http_client.rs
@@ -1,6 +1,12 @@
+use std::{sync::Arc, time::SystemTime};
+
 use hyper::{client::HttpConnector, Client};
 use hyper_proxy::{Proxy as UpstreamProxy, ProxyConnector};
 use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
+use rustls::{
+    client::{ServerCertVerified, ServerCertVerifier},
+    ClientConfig,
+};
 
 #[derive(Clone)]
 pub enum HttpClient {
@@ -10,7 +16,13 @@ pub enum HttpClient {
 
 pub fn gen_client(upstream_proxy: Option<UpstreamProxy>) -> HttpClient {
     let https = HttpsConnectorBuilder::new()
-        .with_webpki_roots()
+        .with_tls_config({
+            let cert_resolver = Arc::new(TrustAllCertVerifier::default());
+            ClientConfig::builder()
+                .with_safe_defaults()
+                .with_custom_certificate_verifier(cert_resolver)
+                .with_no_client_auth()
+        })
         .https_or_http()
         .enable_http1()
         .enable_http2()
@@ -34,5 +46,22 @@ pub fn gen_client(upstream_proxy: Option<UpstreamProxy>) -> HttpClient {
                 .http1_preserve_header_case(true)
                 .build(https),
         )
+    }
+}
+
+#[derive(Default)]
+struct TrustAllCertVerifier;
+
+impl ServerCertVerifier for TrustAllCertVerifier {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &rustls::Certificate,
+        _intermediates: &[rustls::Certificate],
+        _server_name: &rustls::ServerName,
+        _scts: &mut dyn Iterator<Item = &[u8]>,
+        _ocsp_response: &[u8],
+        _n_ow: SystemTime,
+    ) -> Result<ServerCertVerified, rustls::Error> {
+        Ok(ServerCertVerified::assertion())
     }
 }
